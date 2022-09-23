4423246d9d35a16094fad74510159ddc3ac9ed81
pchen03 <peterchen417@gmail.com>
2022-09-22 23:18:23 -0400

    setup scan

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String command = scan.next();
+
+//        switch (command){
+//            case "A":
+//                System.out.println("test");
+//        }
+        System.out.println(command);
+    }
+}
