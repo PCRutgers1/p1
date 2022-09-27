806434036959f1b1799aac83999bb800394ba5f7
pchen03 <peterchen417@gmail.com>
2022-09-27 00:38:24 -0400

    location enum

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,1 +1,19 @@
-public enum Location { } //define the 5 gym locations; refer to Lecture Note #2
+public enum Location {
+    BRIDGEWATER("Bridgewater, 08807, Somerset County"),
+    EDISON("Edison, 08837, Middlesex County"),
+    FRANKLIN("Franklin, 08873, Somerset County"),
+    PISCATAWAY("Piscataway, 08854, Middlesex County"),
+    SOMERVILLE("Somerville, 08876, Somerset County");
+
+    private final String location;
+
+    private Location(String location){
+        this.location = location;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+
+} //define the 5 gym locations; refer to Lecture Note #2
--- src/main/java/MemberDatabase.java
+++ src/main/java/MemberDatabase.java
@@ -1,33 +1,141 @@
 public class MemberDatabase {
-    private Member [] mlist;
+    private Member[] mlist;
     private int size;
-    private int find(Member member) { }
-    private void grow() { }
+
+    private int find(Member member) {
+        for (int i = 0; i < mlist.length; i++) {
+            if (mlist[i] == member) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void grow() {
+        Member[] newList = new Member[this.size + 4];
+        for (int counter = 0; counter < this.size; counter++) {
+            newList[counter] = this.mlist[counter];
+        }
+
+        this.mlist = newList;
+    }
+
+    private boolean checkLocationExists(Member member){
+        Location[] allLocations = Location.values();
+
+        for (Location l : allLocations)
+        {
+            if (l == member.getLocation())
+                return true;
+        }
+        return false;
+    }
+    private boolean checkOver18(Member member){
+        Date today = new Date();
+        int ageYear = today.getYear() - member.getdob().getYear() ;
+        int ageMonth = today.getMonth()- member.getdob().getMonth() ;
+        int ageDay = today.getDay() - member.getdob().getDay() ;
+        if ( ageYear > 18){
+            return true;
+        }else if (ageYear == 18){
+            if (ageMonth < 0){
+                return false;
+            }
+            else if (ageMonth == 0 && ageDay == 0){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     // returns true if member successfully added and false if member is under 18
     public boolean add(Member member) {
-        if (member.getdob().isValid() && member.getExpireDate().isValid() && member.getdob().isValid() && )
+        if (member.getdob().isValid() && member.getExpireDate().isValid() && checkLocationExists(member) && checkOver18(member))
             if (this.mlist.length == 0) {
                 this.mlist = new Member[4];
                 this.size = 1;
                 this.mlist[size - 1] = member;
             } else if (size == this.mlist.length) {
-                Member[] temp = new Member[size + 4];
-                for (int counter = 0; counter < size; counter++) {
-                    temp[counter] = this.mlist[counter];
-                }
-                size++;
-                this.mlist = temp;
+                grow();
+                this.size++;
                 this.mlist[size - 1] = member;
             } else {
                 size++;
                 this.mlist[size - 1] = member;
             }
-
     }
 
-    public boolean remove(Member member) { }
-    public void print () { } //print the array contents as is
-    public void printByCounty() { } //sort by county and then zipcode
-    public void printByExpirationDate() { } //sort by the expiration date
-    public void printByName() { } //sort by last name and then first name
+    public boolean remove(Member member) {
+        int memberIndex = find(member);
+        Member[] newArray = new Member[mlist.length];
+        if (memberIndex == -1) {
+            return false;
+        } else {
+            for (int i = 0; i < this.size; i++) {
+                if (i != memberIndex)
+                    newArray[i] = this.mlist[i];
+            }
+        }
+        size--;
+        this.mlist = newArray;
+        return true;
+    }
+
+    public void print() {
+        for (int i = 0; i < this.size; i++) {
+            System.out.println(mlist[i].toString());
+        }
+    } //print the array contents as is
+
+    public void printByCounty() {
+        for (int ind = 0; ind < this.size; ind++) {
+            System.out.println(this.mlist[ind]);
+        }
+    } //sort by county and then zipcode
+
+    private void countySort() {
+        for (int i = 0; i < this.size - 1; i++)
+            for (int j = 0; j < this.size - i - 1; j++)
+                if (this.mlist[j].getLocation().compareTo(this.mlist[j + 1].getLocation()) > 0) {
+                    // swap arr[j+1] and arr[j]
+                    Member temp = this.mlist[j];
+                    this.mlist[j] = this.mlist[j + 1];
+                    this.mlist[j + 1] = temp;
+                }
+    }
+
+    public void printByExpirationDate() {
+        for (int ind = 0; ind < this.size; ind++) {
+            System.out.println(this.mlist[ind]);
+        }
+    } //sort by the expiration date
+
+    private void dateSort() {
+        for (int i = 0; i < this.size - 1; i++)
+            for (int j = 0; j < this.size - i - 1; j++)
+                if (this.mlist[j].getExpireDate().compareTo(this.mlist[j + 1].getExpireDate()) > 0) {
+                    // swap arr[j+1] and arr[j]
+                    Member temp = this.mlist[j];
+                    this.mlist[j] = this.mlist[j + 1];
+                    this.mlist[j + 1] = temp;
+                }
+    }
+
+    public void printByName() {
+        for (int ind = 0; ind < this.size; ind++) {
+            System.out.println(this.mlist[ind]);
+        }
+    } //sort by last name and then first name
+
+    private void nameSort() {
+        for (int i = 0; i < this.size - 1; i++)
+            for (int j = 0; j < this.size - i - 1; j++)
+                if (this.mlist[j].compareTo(this.mlist[j + 1]) > 0) {
+                    // swap arr[j+1] and arr[j]
+                    Member temp = this.mlist[j];
+                    this.mlist[j] = this.mlist[j + 1];
+                    this.mlist[j + 1] = temp;
+                }
+    }
 }
